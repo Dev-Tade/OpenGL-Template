796a9c570cdcbbd20322e58d59f7e9f5e5d808dd
Dev-Tade <88458312+Dev-Tade@users.noreply.github.com>
2026-01-17 21:32:39 -0300

    Remove duplicate glfwWindowHint of CONTEXT_VERSION_MAJOR

--- src/main.c
+++ src/main.c
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
   }
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   ctx.window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Template", NULL, NULL);
