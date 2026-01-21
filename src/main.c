553767de6060e713c03df441dba3f79ded136437
Dev-Tade <88458312+Dev-Tade@users.noreply.github.com>
2026-01-20 22:02:03 -0300

    Upgrade master to 4.6

--- src/main.c
+++ src/main.c
@@ -50,8 +50,8 @@ int main(int argc, char **argv)
   glfwSetErrorCallback(glfw_error_cb);
   if (!glfwInit()) exit(EXIT_FAILURE);
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   ctx.window = glfwCreateWindow(
